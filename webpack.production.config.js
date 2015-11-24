1b10f92c9b1fbb0d1b8fec6cfc3b4c22ca59c84d
Christian Alfoni <christianalfoni@gmail.com>
2015-11-24 12:11:30 +0100

    Changed name of production file

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
-    filename: '[name]-[hash].min.js'
+    filename: 'main.js'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
