bf8cbb13b32527a3f997af5f12acbc82e8b92e15
Christian Alfoni <christianalfoni@gmail.com>
2016-01-15 12:53:39 +0100

    Removed outdated articles and videos

--- app/Home.js
+++ app/Home.js
@@ -72,26 +72,6 @@ var Home = React.createClass({
                 className="icon icon-play-circle-o link"
                 onClick={this.props.openVideo.bind(null, 'https://www.youtube.com/embed/BfzjuhX4wJ0?start=20690&end=22260')}> ReactiveConf2015 Talk - <small>30:00</small></i>
             </li>
-            <li>
-              <i
-                className="icon icon-file-text link"
-                onClick={() => window.open('http://www.christianalfoni.com/articles/2015_09_22_Introducing-Cerebral')}> Introduction article</i>
-            </li>
-              <li>
-                <i
-                  className="icon icon-play-circle-o link"
-                  onClick={this.props.openVideo.bind(null, 'https://www.youtube.com/embed/O_fk8jBtKSU')}> Introduction - <small>30:37</small></i>
-              </li>
-              <li>
-                <i
-                  className="icon icon-play-circle-o link"
-                  onClick={this.props.openVideo.bind(null, 'https://www.youtube.com/embed/cFB5V86Kz20')}> Get started - <small>31:15</small></i>
-              </li>
-              <li>
-                <i
-                  className="icon icon-play-circle-o link"
-                  onClick={this.props.openVideo.bind(null, 'https://www.youtube.com/embed/PZjXPziD9Cw')}> Routing - <small>15:20</small></i>
-              </li>
             </ul>
           </div>
         </div>
