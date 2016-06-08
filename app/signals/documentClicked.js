88385613b31cb7474f9e4891ba8f7ffe5ae7414d
Christian Alfoni <christianalfoni@gmail.com>
2016-06-08 11:23:03 +0200

    fixed going from front to docuemntation

--- app/signals/documentClicked.js
+++ app/signals/documentClicked.js
@@ -1,5 +1,7 @@
 import copy from 'cerebral-addons/copy';
+import set from 'cerebral-addons/set';
 
 export default [
-  copy('input:/doc', 'state:/currentDocument')
+  copy('input:/doc', 'state:/currentDocument'),
+  set('state:/currentPage', 'documentation')
 ];
